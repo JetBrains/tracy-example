8a23fc3b7e63c814e9b337542c724fea919bfa63
Georgii.Zorabov <georgii.zorabov@jetbrains.com>
2026-03-04 20:36:50 +0700

    Add fallback to console tracing if Langfuse keys are not set

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("com.openai:openai-java:4.16.0")
     implementation("io.opentelemetry:opentelemetry-sdk:$otelVersion")
     implementation("io.opentelemetry:opentelemetry-exporter-otlp:$otelVersion")
+    implementation("io.opentelemetry:opentelemetry-exporter-logging:$otelVersion")
 }
 
 application {
